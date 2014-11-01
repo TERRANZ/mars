d6cc9e7ada4fdde59de67f07d2f1fc8e3c250959
Vadim Korostelev <vterranz@gmail.com>
2014-11-01 18:13:34 +0400

    player stats

--- src/main/java/ru/mars/server/game/Player.java
+++ src/main/java/ru/mars/server/game/Player.java
@@ -5,14 +5,94 @@ package ru.mars.server.game;
  * Time: 14:34
  */
 public class Player {
-    private String name;
-    private Integer level;
+    private String name;//посылать другому
+    private Integer level;//посылать другому
     private Integer strength;
-    private Integer constitution;
+    private Integer constitution; //посылать другому * 10 = здоровье
     private Integer agility;
     private Integer lucky;
-    private Integer weapon;
-    private Integer armor;
+    private Integer weapon;//посылать другому
+    private Integer armor;//посылать другому
     private Integer minDamage;
     private Integer maxDamage;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    public Integer getStrength() {
+        return strength;
+    }
+
+    public void setStrength(Integer strength) {
+        this.strength = strength;
+    }
+
+    public Integer getConstitution() {
+        return constitution;
+    }
+
+    public void setConstitution(Integer constitution) {
+        this.constitution = constitution;
+    }
+
+    public Integer getAgility() {
+        return agility;
+    }
+
+    public void setAgility(Integer agility) {
+        this.agility = agility;
+    }
+
+    public Integer getLucky() {
+        return lucky;
+    }
+
+    public void setLucky(Integer lucky) {
+        this.lucky = lucky;
+    }
+
+    public Integer getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(Integer weapon) {
+        this.weapon = weapon;
+    }
+
+    public Integer getArmor() {
+        return armor;
+    }
+
+    public void setArmor(Integer armor) {
+        this.armor = armor;
+    }
+
+    public Integer getMinDamage() {
+        return minDamage;
+    }
+
+    public void setMinDamage(Integer minDamage) {
+        this.minDamage = minDamage;
+    }
+
+    public Integer getMaxDamage() {
+        return maxDamage;
+    }
+
+    public void setMaxDamage(Integer maxDamage) {
+        this.maxDamage = maxDamage;
+    }
 }
