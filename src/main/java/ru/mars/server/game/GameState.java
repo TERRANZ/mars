758f49b040bcee455cba7b749946acb25e2c5dbc
Vadim Korostelev <vterranz@gmail.com>
2014-11-01 18:01:03 +0400

    player info, game state, messages

---
+++ src/main/java/ru/mars/server/game/GameState.java
@@ -0,0 +1,9 @@
+package ru.mars.server.game;
+
+/**
+ * Date: 01.11.14
+ * Time: 17:09
+ */
+public enum GameState {
+    LOGGED_IN,
+}
--- src/main/java/ru/mars/server/game/Player.java
+++ src/main/java/ru/mars/server/game/Player.java
@@ -5,5 +5,14 @@ package ru.mars.server.game;
  * Time: 14:34
  */
 public class Player {
-
+    private String name;
+    private Integer level;
+    private Integer strength;
+    private Integer constitution;
+    private Integer agility;
+    private Integer lucky;
+    private Integer weapon;
+    private Integer armor;
+    private Integer minDamage;
+    private Integer maxDamage;
 }
