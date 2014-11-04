4f4ea923537904ae792d680c11f1a32ffb4b2605
Vadim Korostelev <vterranz@gmail.com>
2014-11-04 16:55:47 +0400

    map generation

--- src/main/java/ru/mars/server/game/GameThread.java
+++ src/main/java/ru/mars/server/game/GameThread.java
@@ -12,6 +12,7 @@ import ru.mars.server.parser.PlayerParser;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * Date: 01.11.14
@@ -86,7 +87,7 @@ public class GameThread implements Runnable {
     private void initMap() {
         for (int i = 0; i < 8; i++)
             for (int j = 0; j < 8; j++)
-                gemArray[i][j] = 0;
+                gemArray[i][j] = randInt(1, 6);
     }
 
     public int[][] getMap() {
@@ -154,5 +155,28 @@ public class GameThread implements Runnable {
     }
 
     private void moveLineDown(int count) {
+    }
+
+    /**
+     * Returns a pseudo-random number between min and max, inclusive.
+     * The difference between min and max can be at most
+     * <code>Integer.MAX_VALUE - 1</code>.
+     *
+     * @param min Minimum value
+     * @param max Maximum value.  Must be greater than min.
+     * @return Integer between min and max, inclusive.
+     * @see java.util.Random#nextInt(int)
+     */
+    public static int randInt(int min, int max) {
+
+        // NOTE: Usually this should be a field rather than a method
+        // variable so that it is not re-seeded every call.
+        Random rand = new Random();
+
+        // nextInt is normally exclusive of the top value,
+        // so add 1 to make it inclusive
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+
+        return randomNum;
     }
 }
